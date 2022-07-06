d117336ece00818a1d6e4e88f3adb07ef4d899a6
mikasa <akkamannmikasa@gmail.com>
2022-07-06 11:28:54 +0800

    ios14 黑屏问题 hc 特供库
    ios14 黑屏问题 hc 特供库

--- YYKit/Image/YYAnimatedImageView.m
+++ YYKit/Image/YYAnimatedImageView.m
@@ -438,9 +438,23 @@ typedef NS_ENUM(NSUInteger, YYAnimatedImageType) {
 }
 
 - (void)displayLayer:(CALayer *)layer {
-    if (_curFrame) {
-        layer.contents = (__bridge id)_curFrame.CGImage;
-    }
+#pragma mark -- Mikasa hc ios14 黑屏特供修复
+    UIImage *currentFrame = _curFrame;
+
+        if (!currentFrame) {
+            currentFrame = self.image;
+        }
+
+        if (currentFrame) {
+            layer.contentsScale = currentFrame.scale;
+            layer.contents = (__bridge id)currentFrame.CGImage;
+        }
+#pragma mark -- Mikasa hc ios14 黑屏特供修复
+#pragma mark -- base
+//    if (_curFrame) {
+//        layer.contents = (__bridge id)_curFrame.CGImage;
+//    }
+#pragma mark -- base
 }
 
 - (void)setContentsRect:(CGRect)rect forImage:(UIImage *)image{
